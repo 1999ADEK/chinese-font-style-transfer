788a716d87240b753dc1e9f55fb9f22a3bc33e06
1999ADEK <b06201018@ntu.edu.tw>
2021-01-24 00:12:21 +0000

    Add font trainer

---
+++ scripts/download_char_featex.sh
@@ -0,0 +1,2 @@
+# Download dataset
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1vnRPaQDQpC1qXvuo8TM1vHtm6HsNINyN' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1vnRPaQDQpC1qXvuo8TM1vHtm6HsNINyN" -O char_featex.pth && rm -rf /tmp/cookies.txt
